750122e8c96a714b9f6408c6aea48cc326c0b536
CarlosPProjects <cg9822@gmail.com>
2024-12-19 21:53:58 +0100

    Update dependencies to include @prisma/client and refactor TRPC Provider for dynamic API URL resolution
    - Added @prisma/client version 6.1.0 to package.json and package-lock.json.
    - Refactored the TRPC Provider component to dynamically determine the API URL based on the environment, enhancing flexibility for deployment.

--- src/app/_trpc/Provider.tsx
+++ src/app/_trpc/Provider.tsx
@@ -4,11 +4,20 @@ import { trpc } from '@/utils/trpc';
 import { httpBatchLink } from '@trpc/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
+function getBaseUrl() {
+  const base = (() => {
+    if (typeof window !== 'undefined') return '';
+    if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
+    return `http://localhost:${process.env.PORT ?? 3000}`;
+  })();
+  return `${base}/api/trpc`;
+}
+
 const queryClient = new QueryClient();
 const trpcClient = trpc.createClient({
   links: [
     httpBatchLink({
-      url: '/api/trpc',
+      url: getBaseUrl(),
     }),
   ],
 });
