898a583fd4c05ba8601d7322e9671b2242ef00cd
Franceslav <107437303+Franceslav@users.noreply.github.com>
2025-10-15 14:33:22 +0200

    Add Yandex Metrika counter to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 
 import { Toaster } from "sonner";
 
@@ -83,6 +84,34 @@ export default function RootLayout({
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta name="apple-mobile-web-app-title" content="South Park" />
+        
+        {/* Yandex.Metrika counter */}
+        <Script
+          id="yandex-metrika"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(m,e,t,r,i,k,a){
+                m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();
+                for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+              })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=104652080', 'ym');
+
+              ym(104652080, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
+            `,
+          }}
+        />
+        <noscript>
+          <div>
+            <img 
+              src="https://mc.yandex.ru/watch/104652080" 
+              style={{position: 'absolute', left: '-9999px'}} 
+              alt="" 
+            />
+          </div>
+        </noscript>
+        {/* /Yandex.Metrika counter */}
       </head>
       <body
         className="font-sans antialiased"
